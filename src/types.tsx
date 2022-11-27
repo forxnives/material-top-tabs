cntral/react-navigation

feat: update helper types to have navigator specific methods

src/types.tsx
@@ -44,7 +44,8 @@ export type StackNavigationEventMap = {
 export type StackNavigationHelpers = NavigationHelpers<
   ParamListBase,
   StackNavigationEventMap
->;
+> &
+  StackActionHelpers<ParamListBase>;
 
 export type StackNavigationProp<
   ParamList extends ParamListBase,

forxnives/material-top-tabs

src/types.tsx
@@ -37,7 +37,8 @@ export type MaterialTopTabNavigationEventMap = {
 export type MaterialTopTabNavigationHelpers = NavigationHelpers<
   ParamListBase,
   MaterialTopTabNavigationEventMap
->;
+> &
+  TabActionHelpers<ParamListBase>;
 
 export type MaterialTopTabNavigationProp<
   ParamList extends ParamListBase,